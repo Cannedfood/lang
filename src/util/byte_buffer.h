e16b00e53f1890cd6a151e7c9f35e03ec575572e
Benno Straub <benno.straub@outlook.de>
2020-09-27 20:22:08 +0200

    inline inlined definitions

--- src/util/byte_buffer.h
+++ src/util/byte_buffer.h
@@ -16,8 +16,8 @@ void        bb_free(byte_buffer*);
 void        bb_reserve(byte_buffer*, size_t bytes);
 void        bb_append(byte_buffer*,  void const* data, size_t bytes);
 
-static void bb_pushc  (byte_buffer* buffer, char     value) { bb_append(buffer, &value, sizeof(value)); }
-static void bb_pushi32(byte_buffer* buffer, int32_t  value) { bb_append(buffer, &value, sizeof(value)); }
-static void bb_pushf32(byte_buffer* buffer, float    value) { bb_append(buffer, &value, sizeof(value)); }
-static void bb_pushi64(byte_buffer* buffer, uint64_t value) { bb_append(buffer, &value, sizeof(value)); }
-static void bb_pushf64(byte_buffer* buffer, double   value) { bb_append(buffer, &value, sizeof(value)); }
+inline static void bb_pushc  (byte_buffer* buffer, char     value) { bb_append(buffer, &value, sizeof(value)); }
+inline static void bb_pushi32(byte_buffer* buffer, int32_t  value) { bb_append(buffer, &value, sizeof(value)); }
+inline static void bb_pushf32(byte_buffer* buffer, float    value) { bb_append(buffer, &value, sizeof(value)); }
+inline static void bb_pushi64(byte_buffer* buffer, uint64_t value) { bb_append(buffer, &value, sizeof(value)); }
+inline static void bb_pushf64(byte_buffer* buffer, double   value) { bb_append(buffer, &value, sizeof(value)); }
